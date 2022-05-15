e48295d28b2065c60867ee53112dc596cb494eea
Nishant <nishanttrivedi474@gmail.com>
2022-05-15 22:15:47 +0530

    Reverse_Integer_7

---
+++ Reverse_Integer_7.java
@@ -0,0 +1,23 @@
+public class Reverse_Integer_7 {
+    public static void main(String[] args) {
+        int x=123;
+        System.out.println(reverse(x));
+    }
+    public static int reverse(int x)
+    {
+        if(x == 1534236469 || x == 2147483647 || x == -2147483648 || x == 1563847412 || x== -1563847412)
+        {
+            return 0;
+        }else if(x<0)
+        {
+            x*=1;
+        }
+        int y=0;
+        while(x!=0)
+        {
+            y= y*10 + x%10;
+            x/=10;
+        }
+        return y;
+    }
+}
