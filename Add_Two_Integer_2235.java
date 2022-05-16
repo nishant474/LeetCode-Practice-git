d116018e7b529544927af4ca65e99917d4d1fe27
Nishant <nishanttrivedi474@gmail.com>
2022-05-16 11:19:59 +0530

    Add_Two_Integer_2235

---
+++ Add_Two_Integer_2235.java
@@ -0,0 +1,12 @@
+public class Add_Two_Integer_2235 {
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=10;
+        System.out.println(sum(num1,num2));
+    }
+
+    public static int sum(int num1,int num2)
+    {
+        return num1+num2;
+    }
+}
